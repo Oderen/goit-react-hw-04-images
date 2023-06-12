99de08fffb63b5224408a6fc2eaefb74e3876220
Oderen <oleksandr.deren02@gmail.com>
2023-06-12 16:22:51 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -104,53 +104,8 @@ export function App() {
       setStatus('idle');
       return;
     }
-
-    // (function fetchData() {
-    //   const baseURL = 'https://pixabay.com/api';
-    //   const KEY = '36858767-c9bdee91508ce121a2eb6b95d';
-
-    //   fetch(
-    //     `${baseURL}/?q=${searchQuery}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
-    //   )
-    //     .then(response => {
-    //       if (response.ok) {
-    //         return response.json();
-    //       }
-
-    //       return Promise.reject(new Error('Opps, something went wrong'));
-    //     })
-    //     .then(data => {
-    //       const { hits, total, totalHits } = data;
-
-    //       setImages([...images, ...hits]);
-    //       setPictureCount(pictureCount + hits.length);
-    //       setStatus('resolved');
-
-    //       if (total === 0) {
-    //         Notiflix.Notify.info('Sorry, there is no image found');
-    //         return setStatus('rejected');
-    //       }
-
-    //       if (totalHits > pictureCount) {
-    //         setIsButtonVisible(true);
-    //       }
-
-    //       if (totalHits < pictureCount) {
-    //         setIsButtonVisible(false);
-    //         return Notiflix.Notify.info(
-    //           "Wow, look's like these are are all images"
-    //         );
-    //       }
-    //     })
-    //     .catch(error => {
-    //       console.log(error.message);
-    //       setIsButtonVisible(false);
-    //     });
-    // })();
-
-    fetchData();
-
     setStatus('pending');
+    fetchData();
   }, [searchQuery, page]);
   /* eslint-enable */
 
